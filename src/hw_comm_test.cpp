fbc12d6090e2c289b3264800486c4fe7cf92732a
Aaron Wang Shi <aarons.wang@gmail.com>
2014-04-01 17:27:21 +0800

    add i2c gy-86 test to hw_comm_test

--- src/hw_comm_test.cpp
+++ src/hw_comm_test.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <cstdio>
+#include <unistd.h>
 #include <ros/types.h>
 #include "hw_comm/gpio.h"
 #include "hw_comm/i2c.h"
@@ -22,6 +23,34 @@ int main(int argc, char** argv)
     fprintf(stderr, "hello0\n");
 
     i2c.writeByte(MPU6050_ADDR, 0x6B, 0x80);
+    usleep(5 * 1000);
+    i2c.writeByte(MPU6050_ADDR, 0x6B, 0x03);
+    i2c.writeByte(MPU6050_ADDR, 0x19, 0x07);
+    i2c.writeByte(MPU6050_ADDR, 0x1A, 0x06);
+    i2c.writeByte(MPU6050_ADDR, 0x1B, 0x18);
+    i2c.writeByte(MPU6050_ADDR, 0x37, 0x02);
+    usleep(500 * 1000);
+
+    i2c.writeByte(HMC5883L_ADDR, 0x00, 0x010 + 0x01);
+    i2c.writeByte(HMC5883L_ADDR, 0x01, 0x02 << 0x05);
+    i2c.writeByte(HMC5883L_ADDR, 0x02, 0x01);
+    usleep(1000 * 1000);
+    i2c.writeByte(HMC5883L_ADDR, 0x02, 0x01);
+    i2c.writeByte(HMC5883L_ADDR, 0x00, 0x010 + 0x02);
+    i2c.writeByte(HMC5883L_ADDR, 0x02, 0x01);
+    usleep(1000 * 1000);
+    i2c.writeByte(HMC5883L_ADDR, 0x00, 0x70);
+    i2c.writeByte(HMC5883L_ADDR, 0x01, 0x20);
+    i2c.writeByte(HMC5883L_ADDR, 0x02, 0x00);
+
+    i2c.writeByte(MPU6050_ADDR, 0x1C, 0x10);
+    i2c.writeByte(MPU6050_ADDR, 0x6A, 0b00100000);
+    i2c.writeByte(MPU6050_ADDR, 0x37, 0b00000000);
+    i2c.writeByte(MPU6050_ADDR, 0x38, 0b00000001);
+    i2c.writeByte(MPU6050_ADDR, 0x24, 0x0D);
+    i2c.writeByte(MPU6050_ADDR, 0x25, 0x80 | HMC5883L_ADDR);
+    i2c.writeByte(MPU6050_ADDR, 0x26, 0x03);
+    i2c.writeByte(MPU6050_ADDR, 0x27, 0x86);
 
     fprintf(stderr, "hello1\n");
     hw_comm::gpio::HwCommGPIO gpio(4);
