3d194943be42d2b0231e0cfe665fc616ee2bd102
Aaron Wang Shi <aarons.wang@gmail.com>
2014-04-01 17:14:50 +0800

    add i2c test to hw_comm_test

--- src/hw_comm_test.cpp
+++ src/hw_comm_test.cpp
@@ -4,6 +4,10 @@
 #include "hw_comm/gpio.h"
 #include "hw_comm/i2c.h"
 
+#define MPU6050_ADDR  0x68
+#define HMC5883L_ADDR 0x1E
+#define MS6511_ADDR   0x77
+
 struct Handler : public hw_comm::gpio::HwCommGPIOIrqHandler
 {
     virtual void handleIrq()
@@ -14,8 +18,13 @@ struct Handler : public hw_comm::gpio::HwCommGPIOIrqHandler
 
 int main(int argc, char** argv)
 {
+    hw_comm::i2c::HwCommI2C i2c("/dev/i2c-1");
+    fprintf(stderr, "hello0\n");
+
+    i2c.writeByte(MPU6050_ADDR, 0x6B, 0x80);
+
     fprintf(stderr, "hello1\n");
-    hw_comm::gpio::HwCommGPIO gpio(17);
+    hw_comm::gpio::HwCommGPIO gpio(4);
     fprintf(stderr, "hello2\n");
     gpio.setDirection(hw_comm::gpio::GPIO_DIRE_IN);
     fprintf(stderr, "hello3\n");
@@ -27,7 +36,5 @@ int main(int argc, char** argv)
     gpio.waitIrq();
     fprintf(stderr, "hello6\n");
 
-    //    hw_comm::i2c::HwCommI2C i2c("name");
-    fprintf(stderr, "hello7\n");
     return 0;
 }
